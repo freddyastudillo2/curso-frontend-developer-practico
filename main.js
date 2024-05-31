0446ed815bc9e56f216821779186889ab961c6f8
Freddy Astudillo <fastudillo@smartsoft.la>
2024-05-31 15:43:10 -0500

    Integracion componente detalle producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const burguerMenu = document.querySelector('.menu')
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const menuDesktop = document.querySelector('.desktop-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-const menuCarritoCompras = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 
 const cardsContainer = document.querySelector('.cards-container')
 
@@ -13,20 +13,20 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 
 function toggleMenuDesktop(){
-    const isAsideClosed = menuCarritoCompras.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){
-        menuCarritoCompras.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
 
     menuDesktop.classList.toggle('inactive')
 }
 
 function toggleMenuMobile(){
-    const isAsideClosed = menuCarritoCompras.classList.contains('inactive');
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
     
     if(!isAsideClosed){
-        menuCarritoCompras.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
 
     mobileMenu.classList.toggle('inactive')
@@ -39,7 +39,7 @@ function toggleCarritoAside(){
         mobileMenu.classList.add('inactive')
     }
 
-    menuCarritoCompras.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = [];
